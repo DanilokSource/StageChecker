e1d5268df5f0defbd04b91c4a0d1db94e0218db1
DanilokSource <63431565+DanilokSource@users.noreply.github.com>
2022-06-28 21:33:17 +0500

    Update stage.js

--- stage.js
+++ stage.js
@@ -5,14 +5,25 @@ const xhr = new XMLHttpRequest();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 console.log('bot on')
 
-bot.command('serverinfo')
-xhr.open('get', 'https://api.nitestats.com/v1/epic/staging/fortnite')
-xhr.send()
-xhr.onload = () => {
-	const response = xhr.response
-	const stageinfo = (JSON.parse((response)["FortniteStageMain"]["version"]))
-	console.log(stageinfo)
-}
+bot.command('stagecheckupdate', () =>
+	setInterval((ctx) => {
+		xhr.open('get', 'https://api.nitestats.com/v1/epic/staging/fortnite'),
+			xhr.send(),
+			xhr.onload = () => {
+				const serverinfo = (JSON.parse(xhr.response)["FortniteStageMain"]["serverDate"])
+				const serverInfoLast = serverinfo
+				console.log(`Last: ${serverInfoLast}`)
+				console.log(`Now: ${serverinfo}`)
+				if (serverinfo > serverInfoLast)
+					Telegraf.sendMessage(ctx.message.chat.id(`❇️ Сервер Stage получил патч ${stageVersion}. Это значит, что новое обновление полностью готово и должно выйти через неделю, в следующий вторник`))
+				else
+					console.log('Сервер не изменен')
+			}
+	}, 300000)
+
+)
+
+
 
 
 bot.launch()
